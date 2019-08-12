73566c8705ea820520ec2e4b56bc9f4dcec6b823
zhaoheting <492995407@qq.com>
2019-08-12 17:27:57 +0800

    "test"

--- src/main/java/com/programmerFriend/controller/AuthenticateController.java
+++ src/main/java/com/programmerFriend/controller/AuthenticateController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticateController {
 
-    @RequestMapping()
+    @RequestMapping(value = "/login")
     public void test(){
 
     }
